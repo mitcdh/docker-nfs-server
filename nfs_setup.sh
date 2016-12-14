aacdcc3994b319f49645158c14c1acb71833efbd
Mitchell Hewes <me@mitcdh.com>
2016-12-15 08:18:20 +1100

    Defined nfs in /etc/services

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -10,4 +10,9 @@ for mnt in "${mounts[@]}"; do
   echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
 done
 
+cat <<'EOF' >/etc/services
+nfs     2049/tcp    # Network File System
+nfs     2049/udp    # Network File System
+EOF
+
 exec runsvdir /etc/sv
