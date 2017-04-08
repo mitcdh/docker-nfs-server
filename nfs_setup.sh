b008a73e9a8b97fc650ab86cac9e71bf9d66dc2e
Mitchell Hewes <me@mitcdh.com>
2017-04-08 10:10:43 +1000

    Removed noac keyword

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,7 +9,7 @@ rm /etc/exports
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
-  echo "$src *(rw,async,no_subtree_check,no_auth_nlm,no_root_squash,crossmnt,noac,fsid=0)" >> /etc/exports
+  echo "$src *(rw,async,no_subtree_check,no_auth_nlm,no_root_squash,crossmnt,fsid=0)" >> /etc/exports
 done
 
 cat <<'EOF' >/etc/services
